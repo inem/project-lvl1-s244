4cb2157abd559c10cc1d7d715ef7fedc97a48abd
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-04-02 07:48:08 +0200

    v 0.0.5

---
+++ src/bin/brain-even.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import warmWelcome, { askQuestion } from '..';
+
+console.log('Welcome to the Brain Games!');
+
+warmWelcome();
+
+askQuestion();
+askQuestion();
+askQuestion();
--- src/index.js
+++ src/index.js
@@ -5,4 +5,19 @@ const warmWelcome = () => {
   console.log(`Hello, ${name}`);
 };
 
+const correctAnswer = (number) => ((number % 2 === 0) ? 'yes' : 'no');
+
+const explanation = (userAnswer, correctAnswer) => {
+  if (userAnswer === correctAnswer) { return 'Correct!'; }
+  return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
+}
+
+export function askQuestion(maxValue = 20) {
+  const r = Math.floor((Math.random() * maxValue) + 1);
+  console.log(`Question: ${r}`);
+  const userAnswer = readlineSync.question('Answer: ');
+
+  console.log(explanation(userAnswer, correctAnswer(r)));
+};
+
 export default warmWelcome;
